e66714ec80449103751cf5ddd0754f7205eb31e4
awye765 <awye765@googlemail.com>
2019-08-03 10:30:07 +0100

    Moving objects

--- Part 2/moving.py
+++ Part 2/moving.py
@@ -22,25 +22,28 @@ greenSquareY = windowHeight / 2
 #Code Block 3 Variables
 blueSquareX = 0.0
 blueSquareY = 0.0
-blueSquareVX = 1
+blueSquareVX = 8
 blueSquareVY = 1
 
 while True:
 
-	surface.fill((0,0,0))
+	'''
+	# surface.fill((0,0,0))
 
 	# Code block 1 - Drawing Shapes in Time and Space
-	pygame.draw.rect(surface, (255,0,0), (random.randint(0, windowWidth), random.randint(0, windowHeight), 10, 10 ) )
+	#pygame.draw.rect(surface, (255,0,0), (random.randint(0, windowWidth), random.randint(0, windowHeight), 10, 10 ) )
 
 	#Code Block 2 - Moving Shapes Across Space in Time
-	'''
+
+
 	pygame.draw.rect(surface, (0, 255, 0), (greenSquareX, greenSquareY, 10, 10))
 
-	greenSquareX += 1
+	greenSquareX += 5
 	#greenSquareY += 1
+
 	'''
 	#Code block 3 - Moving Shapes with Acceleration
-	'''
+
 	pygame.draw.rect(surface, (0, 0, 255), (blueSquareX, blueSquareY, 10, 10))
 
 	blueSquareX += blueSquareVX
@@ -48,7 +51,6 @@ while True:
 
 	blueSquareVX -= 0.2
 	blueSquareVY += 0.1
-	'''
 
 	for event in GAME_EVENTS.get():
 
